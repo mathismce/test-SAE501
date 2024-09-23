1119d9abbc2b0c607c9e9b5c49a8ee36ca5a3e7c
Mathis Micheau <mathis.micheau@etu.unilim.fr>
2024-09-23 11:55:43 +0200

    23/09

--- test-front/app/page.tsx
+++ test-front/app/page.tsx
@@ -1,8 +1,25 @@
+import { Button } from "@/components/ui/button"
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
-    <div className="">
-      <h2 className="">Hello world</h2>
+    <div className="flex flex-col gap-4 justify-center items-center p-12">
+      <div className="flex gap-4">
+        <Button size="sm" variant="default">Create</Button>
+        <Button size="sm" variant="secondary">Cancel</Button>
+        <Button size="sm" variant="outline">Cancel</Button>
+        <Button size="sm" variant="ghost">Create</Button>
+        <Button size="sm" variant="destructive">Create</Button>
+        <Button size="sm" variant="link">Create</Button>
+      </div>
+      <div className="flex gap-2">
+      <Button size="sm" variant="default">Create</Button>
+      <Button size="sm" variant="outline">Cancel</Button>
+      <Button size="sm" variant="outline" className="gap-2">
+        New event
+        <ArrowRightIcon className="h-6 w-4 text-gray-500" />
+      </Button>
+      </div>
     </div>
   );
 }
